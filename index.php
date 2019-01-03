f4dc30f2723d65d419ce0ee4c77822a4b3fd67bd
VIRUS471 <45210574+VIRUS471@users.noreply.github.com>
2019-01-04 01:12:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Login</title>
-  </head>
-  <body>
 
-  </body>
-</html>
+<?php
+include_once 'conn.php';
+
+
+
+$username = "123";
+$password = "123";
+
+function checkUser($username, $password){
+  $db;
+  $table = "users";
+
+  $query = "SELECT * FROM".$this->table."WHERE username=''$username' and password='$password' limit 1";
+
+  $result = mysqli_query($this->db->getDb(), $query);
+
+  if(mysqli_num_rows($result)){
+    mysqli_close($this->db-getDb());
+
+
+    printf("<h1>GOOD</h1>");
+  }
+  else{
+    printf("<h1>GOOD</h1>");
+  }
+
+
+}
+?>
